57d019288935bb1d8e41b313b0f832a31cb1237e
nasu0210 <86135357+nasu0210@users.noreply.github.com>
2025-07-15 10:08:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "안녕, 무극의 세계!"
+    return b'gAAAAABodanaU8f4ZQZA_8efsXhdOsRrSdXg7XpRgbZ7PvOY7_ajBEv8x0JbBlM8RUGM6NZqJrbrqzMWyCdklQ3sMVC7hFSCyV-xSG053In4NdLgfGjEXbs='
 
 if __name__ == '__main__':
     app.run()
